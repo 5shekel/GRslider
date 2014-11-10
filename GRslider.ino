15b1be7148d264b5aa8f4dcbbbd8a7bbfaff7440
5shekel <yair99@gmail.com>
2014-11-11 01:32:12 +0200

    ready to merge

--- GRslider.ino
+++ GRslider.ino
@@ -9,8 +9,7 @@
 //STEPPER PROPERTIES
 
 //////// #1 //////////////
-int stepper1_Speed = 5000;
-int stepper1_accl = 4500;
+int stepper1_Speed = 400;
 
 #define limitPin1a 5
 #define limitPin1b 4
@@ -69,14 +68,15 @@ enum
   kKnob           , //translate knob to motion
   kGoStepper1           , //pluck string
 };
+
 // Callbacks define on which received commands we take action
-void attachCommandCallbacks()
-{
+void attachCommandCallbacks(){
   // Attach callback methods
   cmdMessenger.attach(OnUnknownCommand);
   cmdMessenger.attach(kCommandList, OnCommandList);
-  cmdMessenger.attach(kSetSpeed, OnSetSpeed);
-  cmdMessenger.attach(kSetAccel, OnSetaccel);
+  cmdMessenger.attach(kSetSpeed, OnSetSpeed1);
+  cmdMessenger.attach(kSetSpeed, OnSetSpeed2);
+  cmdMessenger.attach(kSetAccel, OnSetaccel2);
   cmdMessenger.attach(kGoStepper2, onGo2);
   cmdMessenger.attach(kHome, homeAll);
   cmdMessenger.attach(kSleep, sleepAll);
@@ -84,47 +84,46 @@ void attachCommandCallbacks()
   cmdMessenger.attach(kGoStepper1, onGo1);
 }
 // Called when a received command has no attached function
-void OnUnknownCommand()
-{
+void OnUnknownCommand(){
   Serial.println("This command is unknown!");
   ShowCommands();
 }
 
 // Callback function that shows a list of commands
-void OnCommandList()
-{
-  ShowCommands();
-}
+void OnCommandList(){  ShowCommands();}
 
 // Show available commands
 void ShowCommands() 
 {
   Serial.println("Available commands");
   Serial.println(" 0;                 - This command list");
-  Serial.println(" 1,<set speed stepper>;     - Set speed. 100-25600");
-  Serial.print  (" 2,<set accelaration>; - Set acceleration 100-25600 "); 
-  Serial.println(" 3;                  - go stepper 2");
-  Serial.println(" 4;                  - home stepper");
-  Serial.println(" 5;                  - sleep stepper");
-  Serial.println(" 6;                  - knob controller");
-  Serial.println(" 7;                  - pick movement (stepper 1)");
+  Serial.println(" 1,<set speed stepper1>;     - Set speed. 50-550");
+  Serial.println(" 2,<set speed stepper2>;     - Set speed. 100-25600");
+  Serial.print  (" 3,<set accelaration>; - Set acceleration 100-25600 "); 
+  Serial.println(" 4;                  - go stepper 2");
+  Serial.println(" 5;                  - home stepper");
+  Serial.println(" 6;                  - sleep stepper");
+  Serial.println(" 7;                  - knob controller");
+  Serial.println(" 8;                  - pick movement (stepper 1)");
   showVals();
 }
 
-void OnSetaccel(){
-  stepper1_accl = cmdMessenger.readInt16Arg(); 
+void OnSetaccel2(){
   stepper2_accl = cmdMessenger.readInt16Arg(); 
   showVals();
 }
 
-void OnSetSpeed(){
+void OnSetSpeed1(){
   stepper1_Speed = cmdMessenger.readInt16Arg(); 
+  showVals();
+}
+
+void OnSetSpeed2(){
   stepper2_Speed = cmdMessenger.readInt16Arg(); 
   showVals();
 }
 
-void onGo1()
-{
+void onGo1(){
   //we call this to initiate the stepper 1 action
  // stepper1.enableOutputs();
   stepperMove = 1;
@@ -159,40 +158,36 @@ void knobEnable(){
 }
 
 void knobControl(){
-    stepper2.enableOutputs();
-    stepper2.setMaxSpeed(1000);
-    stepper2.setAcceleration(10000);
+    if(knobGo) {
+      stepper2.enableOutputs();
+      stepper2.setMaxSpeed(1000);
+      stepper2.setAcceleration(10000);
 
-    if (millis() > (lastMeasurementTime + measurementInterval)) {
-      sensorValue = analogRead(sensorPin);
-      sensorValue = map(sensorValue, 0 ,1024, 0, 800);
-      
-      total= total - readings[readIndex];         
-      readings[readIndex] = sensorValue;
-      total= total + readings[readIndex];       
-      readIndex = readIndex + 1;                    
-      if (readIndex >= numReadings)              
-        readIndex = 0;                           
+      if (millis() > (lastMeasurementTime + measurementInterval)) {
+        sensorValue = analogRead(sensorPin);
+        sensorValue = map(sensorValue, 0 ,1024, 0, 800);
+        
+        total= total - readings[readIndex];         
+        readings[readIndex] = sensorValue;
+        total= total + readings[readIndex];       
+        readIndex = readIndex + 1;                    
+        if (readIndex >= numReadings)              
+          readIndex = 0;                           
 
-      average = total / numReadings; 
+        average = total / numReadings; 
 
-      stepper2.moveTo(average);
-      Serial.println(average);
+        stepper2.moveTo(average);
+        Serial.println(average);
 
-      lastMeasurementTime = millis();
+        lastMeasurementTime = millis();
+      }
     }
 }
 
 void showVals(){
-  Serial.print("current speed 1: ");
-  Serial.println(stepper1_Speed);
-  Serial.print("current accel 1: ");
-  Serial.println(stepper1_accl);   
-  Serial.println(" ");
-  Serial.print("current speed 2: ");
-  Serial.println(stepper2_Speed);
-  Serial.print("current accel 2: ");
-  Serial.println(stepper2_accl); 
+  Serial<<"current speed 1: "<<stepper1_Speed<<endl;
+  Serial<<"current speed 2: "<<stepper2_Speed<<endl;
+  Serial<<"current accel 2: "<<stepper2_accl<<endl; 
 }
 
 
@@ -203,16 +198,14 @@ void stepper1_action(){
     {
       limit_A = 2;
       stepperMove = false;
-      stepper1.setSpeed(400);
-      Serial.println("stop on limit_a");
+      stepper1.setSpeed(stepper1_Speed);
       stepper1.disableOutputs();
     }
     else if(!digitalRead(limitPin1b) && limit_A==2)
     {
       limit_A = 1;
       stepperMove = false;
-      stepper1.setSpeed(-400);
-      Serial.println("stop on limit_b");
+      stepper1.setSpeed(-stepper1_Speed);
       stepper1.disableOutputs();
     }
     stepper1.runSpeed();
@@ -229,9 +222,7 @@ void randStepper2(){
 }
 
 
-void setup()
-{
-
+void setup(){
   /////// stepper 1 ////////
   stepper1.setPinsInverted(14,0,1); //dir, step, enable
   stepper1.setMaxSpeed(stepper1_Speed);
@@ -268,16 +259,14 @@ void setup()
   //run steppers until they hits the limit switch   
   homeStepper1();
   homeStepper2();     
-
 }
 
-void loop()
-{
+void loop(){
   // Process incoming serial data, and perform callbacks
   if(DEBUG) cmdMessenger.feedinSerialData();
 
   //if mode 6, translate analog pin(A5) to motion
-  if(knobGo) knobControl();
+  knobControl();
   
   stepper1_action();
   stepper2.run(); // slider stepper 02
@@ -288,29 +277,23 @@ void loop()
 
 ////////// STEPPER SEQ ///////////
 
-void homeStepper1()
-{
+void homeStepper1(){
   //run stepper until it hits the limit switch
   //the while is BLOCKING so we cant use it in main loop
   if(DEBUG)Serial.println("home_01");
 
   stepper1.enableOutputs();
-  stepper1.setSpeed(400);
+  stepper1.setSpeed(stepper1_Speed);
   while(digitalRead(limitPin1b))
     stepper1.runSpeed();
 
-  if(DEBUG)Serial.println("steps:"); //how many steps did we take to reach home
-  if(DEBUG)Serial.println(stepper1.currentPosition());
-
-  // prime stepper for the action
-  // for some reason runSpeed() doesnt work, so we do use acceleration.
+  // prime stepper for next action
   limit_A = 1;
-  stepper1.setSpeed(-400);
+  stepper1.setSpeed(-stepper1_Speed);
   stepper1.disableOutputs();
 }
 
-void homeStepper2()
-{
+void homeStepper2(){
   //run stepper until it hits the limit switch
   //the while is BLOCKING so we cant use it in main loop
   if(DEBUG)Serial.println("home_02");
